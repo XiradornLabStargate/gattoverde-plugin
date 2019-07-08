98586f57ca9b15846fa52cb79a41b7a1f30f60a6
Xiradorn <xiradorn@gmail.com>
2019-07-08 23:47:18 +0200

    incomplete

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -14,11 +14,12 @@ class Admin extends BaseController
 	public $settings;
 
 	public $pages = array();
+
+	public $subpages = array();
 	
 	public function __construct() {
 		$this->settings = new SettingsApi();
 
-		// add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
 		$this->pages = array(
 			array(
 				'page_title' 	=> 'GattoVerde Plugin', 
@@ -28,48 +29,41 @@ class Admin extends BaseController
 				'callback' 		=> function() { echo "<h1>GattoVerde Plugin</h1>"; }, 
 				'icon_url' 		=> 'dashicons-store', 
 				'position' 		=> 110
+			)
+		);
+
+		$this->subpages = array(
+			array(
+				'parent_slug' 	=> 'gattoverde_plugin', 
+				'page_title' 	=> 'Custom Post Type', 
+				'menu_title' 	=> 'CPT', 
+				'capability' 	=> 'manage_options', 
+				'menu_slug' 	=> 'gattoverde_cpt', 
+				'callback' 		=> function() { echo "<h1>GattoVerde CPT</h1>"; },
 			),
 			array(
-				'page_title' 	=> 'GattoVerde Plugin 1', 
-				'menu_title' 	=> 'GattoVerde1', 
+				'parent_slug' 	=> 'gattoverde_plugin', 
+				'page_title' 	=> 'GattoVerde Taxonomies', 
+				'menu_title' 	=> 'Taxonomies', 
 				'capability' 	=> 'manage_options', 
-				'menu_slug' 	=> 'gattoverde_plugin_1', 
-				'callback' 		=> function() { echo "<h1>GattoVerde Plugin 1</h1>"; }, 
-				'icon_url' 		=> 'dashicons-external', 
-				'position' 		=> 10
-			)
+				'menu_slug' 	=> 'gattoverde_taxonomies', 
+				'callback' 		=> function() { echo "<h1>GattoVerde CPT</h1>"; },
+			),
+			array(
+				'parent_slug' 	=> 'gattoverde_plugin', 
+				'page_title' 	=> 'GattoVerde Widgets', 
+				'menu_title' 	=> 'Widgets', 
+				'capability' 	=> 'manage_options', 
+				'menu_slug' 	=> 'gattoverde_widgets', 
+				'callback' 		=> function() { echo "<h1>GattoVerde CPT</h1>"; },
+			),
 		);
 	}
 
 	// used for registering the services inside a special class
 	public function register() 
 	{
-		// add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-		
-		// i can add pages into the constructor
-		// $pages = [
-		// 	[
-		// 		'page_title' 	=> 'GattoVerde Plugin', 
-		// 		'menu_title' 	=> 'GattoVerde', 
-		// 		'capability' 	=> 'manage_options', 
-		// 		'menu_slug' 	=> 'gattoverde_plugin', 
-		// 		'callback' 		=> function() { echo "<h1>GattoVerde Plugin</h1>"; }, 
-		// 		'icon_url' 		=> 'dashicons-store', 
-		// 		'position' 		=> 110
-		// 	],
-		// 	[
-		// 		'page_title' 	=> 'GattoVerde Plugin 1', 
-		// 		'menu_title' 	=> 'GattoVerde', 
-		// 		'capability' 	=> 'manage_options', 
-		// 		'menu_slug' 	=> 'gattoverde_plugin_1', 
-		// 		'callback' 		=> function() { echo "<h1>GattoVerde Plugin 1</h1>"; }, 
-		// 		'icon_url' 		=> 'dashicons-extrenal', 
-		// 		'position' 		=> 11
-		// 	]
-		// ];
-
-		// $this->settings->addPages( $pages )->register();
-		$this->settings->addPages( $this->pages )->register();
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 
 	// public function add_admin_pages() 
