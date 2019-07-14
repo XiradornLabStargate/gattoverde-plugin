ff540ba844d0689d44f5478687cfe7aab8b7b6c1
Xiradorn <xiradorn@gmail.com>
2019-07-14 21:47:04 +0200

    Just a fix for basic stuff on activation

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -10,5 +10,14 @@ class Activate
 	public static function activate() 
 	{
 		flush_rewrite_rules();
+
+		// prevent false override
+		if ( get_option( 'gattoverde_plugin' ) ) {
+			return;
+		}
+
+		// default array for zero stuff on first activation
+		$default_ary = array();
+		update_option( 'gattoverde_plugin', $default_ary );
 	}
 }
